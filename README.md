51785024e6ede378a8e1d7eb06f21d6403f0ba1d
João Manieri <71887875+JoaoManieri@users.noreply.github.com>
2023-03-02 23:20:30 -0300

    Improvements in the creation of new discussions, added new parameters

---
+++ README.md
@@ -0,0 +1,8 @@
+# Projeto Ipe-Branco
+## Sobre: 
+Porque Ipê-Branco? Eu decidi que vou nomear meus projetos com nomes populares e nomes científicos de árvores típicas da fauna brasileira (Meu país de origem) e em homenagem a minha futura esposa Beatriz que é bióloga! 
+
+Do que se trata o projeto: O projeto Ipê-Branco é um app de smartfones com sistema operacional android que tem por objetivo final servir como um fórum de reparadores automotivos para que estes possa participar de uma comunidade harmoniosa trocando informações visando melhorar o trabalho do dia a dia dessas pessoas 
+
+<img src="doc/print.jpeg">
+
--- app/src/main/java/br/com/manieri/ipe_branco/ui/main/Discussion/InitDiscussionFragment.kt
+++ app/src/main/java/br/com/manieri/ipe_branco/ui/main/Discussion/InitDiscussionFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -36,6 +37,13 @@ class InitDiscussionFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val assuntos = arrayOf("Escolha um problema","Produto Tecnomtor", "Codigo de defeito", "Problema mecanico")
+        binding.spinnerAssunto
+        val adapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_spinner_dropdown_item, assuntos)
+        binding.spinnerAssunto.adapter = adapter
+
+
         viewModel = ViewModelProvider(this)[DiscussionViewModel::class.java]
 
         binding.sendQuestion.setOnClickListener {
